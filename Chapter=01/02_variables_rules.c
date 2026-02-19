5eaf3094af9e0263cb2303a42f09a98857514d39
Deevanshu <deevanshu123@gmail.com>
2026-02-19 12:05:32 +0530

    today work

--- Chapter=01/02_variables_rules.c
+++ Chapter=01/02_variables_rules.c
@@ -4,21 +4,13 @@ int main() {
     int a = 30; 
     int A = 40;
 
-    int _age = 23;
+    int _age = 24;
     int age2 = 24;
+    age =26;
 
     int final_price = 100;
     
-    printf("Value of a: %d\n", a);
-    scanf("%d", &a);
-    printf("Value of A: %d\n", A);
-    scanf("%d", &A);  
-    printf("Value of _age: %d\n", _age);
-    scanf("%d", &_age);
-    printf("Value of age2: %d\n", age2);
-    scanf("%d", &age2);
-    printf("Value of final_price: %d\n", final_price);
-    scanf("%d", &final_price);
-
+        printf(");
     return 0;
 }
+
