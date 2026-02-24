ae572a8d178eb5014f2ce8ba0983f74d1d4deed0
Deevanshu <deevanshu123@gmail.com>
2026-02-24 19:51:54 +0530

    sync deletions

--- 62_inititalization.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float price[]={97.0,98.0,89.0};
-
-    printf("total price 1: %f\n", price[0]+(0.18*price[0]));
-    printf("total price 2: %f\n", price[1]+(0.18*price[1]));
-    printf("total price 3: %f\n", price[2]+(0.18*price[2]));
-    return 0;
-}
